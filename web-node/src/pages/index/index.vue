cb5ed8b0fc25ad95cc1b73e1b03f4806dc2ccabe
xiangjiejie <xiangjiejie@duiba.com.cn>
2020-03-08 18:59:09 +0800

    feat: 后端框架搭建

---
+++ web-node/src/pages/index/index.vue
@@ -0,0 +1,150 @@
+<template>
+<div>
+  <div class="title">请输入一段文本：</div>
+<el-input
+  type="text"
+  placeholder="请输入标题"
+  maxlength="50"
+  class="title-input"
+  v-model="title">
+    <template slot="prepend">标题</template>
+
+</el-input>
+   <el-input
+  type="textarea"
+  :autosize="{ minRows: 6, maxRows: 12}"
+  placeholder="请输入文本内容"
+  maxlength="2000"
+  show-word-limit
+  v-model="content">
+  <template slot="prepend">文本</template>
+</el-input>
+<div class="button-wrap">
+  <el-button v-show="status !== STATUS.PROCESS" @click="handleAnaly" type="primary" plain>开始分析</el-button>
+  <!-- <el-button v-show="status !== STATUS.PROCESS" @click="test" type="primary" plain>开始分析get</el-button> -->
+  <!-- <el-button v-show="status !== STATUS.PROCESS" @click="testPost" type="primary" plain>开始分析post</el-button> -->
+</div>
+<div class="process" v-show="status === STATUS.PROCESS">
+  <el-progress type="line" :percentage="percent" text-inside
+  :stroke-width="30"
+  show-text
+  ></el-progress>
+  <center>分析中...</center>
+</div>
+<!-- v-show="status === STATUS.completed"  -->
+<div v-show="status === STATUS.COMPLET" class="result">
+  <div class="result-title">自动摘要结果</div>
+  <div class="result-content">
+  {{result}}
+  </div>
+</div>
+</div>
+</template>
+
+<script>
+import axios from 'axios'
+const STATUS = {
+  READY: 0,
+  PROCESS: 1,
+  COMPLET: 2
+}
+export default {
+  data () {
+    return {
+      title: '',
+      content: '',
+      result: '',
+      status: STATUS.READY,
+      percent: 0,
+      completed: false,
+      STATUS: STATUS
+
+    }
+  },
+  methods: {
+    async handleAnaly () {
+      this.timeStep = 200
+
+      this.timer = () => setTimeout(() => {
+        this.percent = this.percent + 1
+        if (this.percent > 30) {
+          this.timeStep = 250
+        }
+        if (this.percent > 50) {
+          this.timeStep = 300
+        }
+        if (this.percent < 95) {
+          this.timer()
+        }
+      }, this.timeStep)
+      this.timer()
+
+      this.status = STATUS.PROCESS
+      // const res = await axios.post('//localhost:8080', {
+      //   content: this.content,
+      //   title: this.title
+      // })
+      try {
+        const res = await axios.get(`//${location.host}:8080/index`, {
+          params: {
+            content: this.content,
+            title: this.title
+          }
+        })
+        this.result = res.data.data
+        this.status = STATUS.COMPLET
+      } catch (e) {
+        this.$message.error(JSON.stringify(e.stack))
+        this.status = STATUS.COMPLET
+      }
+    }
+    // async test () {
+    //   const res = await axios.get('//localhost:8080/index', {
+    //     params: {
+    //       content: this.content,
+    //       title: this.title
+    //     }
+    //   })
+    // },
+    // async testPost () {
+    //   const res = await axios.post('//localhost:8080/index', {
+    //     content: this.content,
+    //     title: this.title
+    //   })
+    // }
+  }
+}
+</script>
+
+<style  scoped>
+.title{
+  font-weight: 600;
+  padding: 20px 0;
+}
+.title-input{
+  margin-bottom: 15px;
+}
+.button-wrap{
+  margin: 50px 0;
+  text-align: center;
+}
+
+.result{
+  padding: 0 10px;
+  border-radius: 4px;
+  border:1px solid #dcdfe6;
+}
+.result-title{
+  padding: 10px 0;
+  font-weight: 600;
+  border-bottom: 1px solid #dcdfe6;
+}
+.result-content{
+  padding: 10px 0;
+}
+.process{
+  width:500px;
+  margin: 0 auto 50px auto;
+}
+
+</style>
