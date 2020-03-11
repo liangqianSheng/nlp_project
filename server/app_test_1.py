0838e65043035c03fe918b43764c759afbbf4f74
xiangjiejie <xiangjiejie@duiba.com.cn>
2020-03-11 22:05:31 +0800

    feat: 测试增加文件

--- server/app_test_1.py
+++ server/app_test_1.py
@@ -1,16 +1,13 @@
 # coding:utf-8
 import numpy as np
 import pickle
-# word_vector = np.load('./words_vector.npy',allow_pickle= True)
-# word_dict = {}
-# for i in range(len(word_vector)):
-#     word_dict[word_vector[i][0]]=word_vector[i][1]
+
 
 word_dict=pickle.load(open('./words_vector_after.txt', 'rb'))
 
 
 
-# articles=pickle.load(open('./articles.txt', 'rb'))
+articles=pickle.load(open('./articles.txt', 'rb'))
 # titles=pickle.load(open('./titles.txt', 'rb'))
 
 
